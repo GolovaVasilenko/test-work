557858a2dc785d17b8ba418b1aba44262e3e8704
GolovaVasilenko <vasilenko75@gmail.com>
2021-01-30 18:10:43 +0200

    added intrfaces

---
+++ app/Interfaces/TallManagerInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Interfaces;
+
+interface TallManagerInterface
+{
+    public function canTalkManager();
+}
--- app/Tester.php
+++ app/Tester.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tester extends Model
 {
-    //
+    public $skills = [  'writeCode', 'testCode', 'talkManager'];
 }
